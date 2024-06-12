493d4cce18c39d257d73192f578f0ef53dbe1c38
Max Komarychev <maxkomarychev@gmail.com>
2024-06-12 12:05:11 +0200

    Add federation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { GraphQLModule } from "@nestjs/graphql";
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from "@nestjs/apollo";
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: { federation: 2, path: "auto.graphql" },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
